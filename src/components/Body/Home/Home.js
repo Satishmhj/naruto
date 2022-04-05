37898909a27f248ba061b568c7be30927025765d
sateesh <sateeshmhj1@gmail.com>
2022-04-05 12:49:20 +0545

    feat: image slider using react-bootstrap

--- src/components/Body/Home/Home.js
+++ src/components/Body/Home/Home.js
@@ -1,18 +1,45 @@
+import { Carousel } from "react-bootstrap";
 import React from "react";
-import "./home.css"
+import "./home.css";
 
 const Home = () => {
   return (
     <>
-      <div class="banner">
-        <div class="banner-text">
-          {/* <p>
-            <p className="text-black">Welcome to Naruto World </p>
-          </p> */}
-        </div>
-        <div class="banner-img">
-          {/* <img src="./assets/images/slider-img.png" alt="hero-background" /> */}
-        </div>
+      <div>
+        <Carousel>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="https://www.teahub.io/photos/full/180-1808332_naruto-and-friends-wallpaper-best-wallpaper-anime-naruto.jpg"
+              alt="First slide"
+            />
+            <Carousel.Caption>
+              <h3>Uzumaki Naruto and his family.</h3>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="https://i.pinimg.com/originals/3c/99/9c/3c999cd310f95f8878f38589b7ac8167.jpg"
+              alt="Second slide"
+            />
+
+            <Carousel.Caption>
+              <h3>Uzumaki Naruto and Nine tails.</h3>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="https://wallpaperaccess.com/full/229970.jpg"
+              alt="Third slide"
+            />
+
+            <Carousel.Caption>
+              <h3>Naruto shipudden</h3>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
       </div>
     </>
   );
